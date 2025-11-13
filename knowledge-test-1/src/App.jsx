c9e689d35be8662a611b1d28edbf066e84aff15b
Dylan S <dsand171@mtroyal.ca>
2025-11-13 16:10:55 -0700

    updated with add favourite function. need to implement further

--- knowledge-test-1/src/App.jsx
+++ knowledge-test-1/src/App.jsx
@@ -1,7 +1,7 @@
-import { useState } from 'react'
-import movieDataJSON from "./data/movie-data"
-import { Header } from './components/Header';
-import { MovieList } from './components/MovieList';
+import { useState } from "react";
+import movieDataJSON from "./data/movie-data";
+import { Header } from "./components/Header";
+import { MovieList } from "./components/MovieList";
 
 function App() {
   // Load movie data and have a copy of it.
@@ -9,39 +9,56 @@ function App() {
   const copyOfMovieData = movieData;
 
   // Initialize favourites as empty.
-  const [favourites, setFavourites] = useState([{   id: 128, title: "Past Lives", tagline: "Love and Destiny",
-        poster: "/k3waqVXSnvCZWfJYNtdamTgTtTA.jpg", runtime: 106, tmdbID: 666277
+  const [favourites, setFavourites] = useState([
+    {
+      id: 128,
+      title: "Past Lives",
+      tagline: "Love and Destiny",
+      poster: "/k3waqVXSnvCZWfJYNtdamTgTtTA.jpg",
+      runtime: 106,
+      tmdbID: 666277,
     },
-    {   id: 180, title: "Oppenheimer", tagline: "Now I am Become Death, the Destroyer of Worlds",
-        poster: "/8Gxv8gSFCU0XGDykEGv7zR1n2ua.jpg", runtime: 180, tmdbID: 872585
+    {
+      id: 180,
+      title: "Oppenheimer",
+      tagline: "Now I am Become Death, the Destroyer of Worlds",
+      poster: "/8Gxv8gSFCU0XGDykEGv7zR1n2ua.jpg",
+      runtime: 180,
+      tmdbID: 872585,
     },
-   {   id: 229, title: "Killers of the Flower Moon ", tagline: "When oil is discovered in 1920s Oklahoma under Osage Nation land, the Osage people are murdered one by one — until the FBI steps in to unravel the mystery",
-        poster: "/dB6Krk806zeqd0YNp2ngQ9zXteH.jpg", runtime: 206, tmdbID: 466420
+    {
+      id: 229,
+      title: "Killers of the Flower Moon ",
+      tagline:
+        "When oil is discovered in 1920s Oklahoma under Osage Nation land, the Osage people are murdered one by one — until the FBI steps in to unravel the mystery",
+      poster: "/dB6Krk806zeqd0YNp2ngQ9zXteH.jpg",
+      runtime: 206,
+      tmdbID: 466420,
     },
-    {   id: 2, title: "La Dolce Vita", tagline: "The world's most talked about movie today",
-        poster: "/11hMHhThUb53fHoDDyn5LUsw0Z.jpg", runtime: 174, tmdbID: 439
-    },     ]); //empty array
-
+    {
+      id: 2,
+      title: "La Dolce Vita",
+      tagline: "The world's most talked about movie today",
+      poster: "/11hMHhThUb53fHoDDyn5LUsw0Z.jpg",
+      runtime: 174,
+      tmdbID: 439,
+    },
+  ]); //empty array
 
   // Update the favourites array.
   const addToFavourites = (movie) => {
-      const handler = () => {}
-    //TODO
-  }
-  
+    alert("in process of fav'ing!");
+    setFavourites([...favourites, movie]);
+  };
 
   return (
     <>
       <article className="container">
-        <Header favourites={favourites}/>
-        <MovieList movies={copyOfMovieData}/>
+        <Header favourites={favourites} />
+        <MovieList movies={copyOfMovieData} addToFavourites={addToFavourites} />
       </article>
-
-      
-    
     </>
-    
-  )
+  );
 }
 
-export default App
+export default App;
--- knowledge-test-1/src/components/MovieList.jsx
+++ knowledge-test-1/src/components/MovieList.jsx
@@ -1,16 +1,20 @@
 import { SingleMovie } from "./SingleMovie";
 
 export const MovieList = (props) => {
-
-    return(
-        <ul className="columns is-multiline">
-
-                     {props.movies.map(m => {
-                        
-                        return <SingleMovie key={m.id} poster={m.poster} title={m.title} tagline={m.tagline}/>
-                    })}
-        </ul>
-
-
-    );
-}
+  return (
+    <ul className="columns is-multiline">
+      {props.movies.map((m) => {
+        return (
+          <SingleMovie
+            key={m.id}
+            poster={m.poster}
+            title={m.title}
+            tagline={m.tagline}
+            singleMovieObject={m}
+            addToFavourites={props.addToFavourites}
+          />
+        );
+      })}
+    </ul>
+  );
+};
